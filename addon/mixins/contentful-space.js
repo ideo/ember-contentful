672d1436081777567301ebadb24e4e1c4d4515f3
Marcus Paeschke <marcus@mphasize.de>
2015-04-29 14:30:36 +0200

    Adding a `contentful-space` mixin.

---
+++ addon/mixins/contentful-space.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+
+var memcache = {};
+
+var ContentfulMixin = Ember.Mixin.create({
+
+  connectSpace: function() {
+    var spaceId = this.get('session.contentful_space');
+
+    if (!spaceId) {
+      return new Ember.RSVP.Promise(function(resolve, reject) {
+        reject("No session or missing contentful_space ID in session data.");
+      });
+    }
+
+    if (memcache[spaceId]) {
+      return memcache[spaceId];
+    }
+
+    var accessToken = this.get('session.access_token');
+    var client = this.contentfulManagement.createClient({
+      accessToken: accessToken,
+      secure: true
+    });
+
+    var promise = client.getSpace(spaceId);
+    memcache[spaceId] = promise;
+    return promise;
+  }
+});
+
+export
+default ContentfulMixin;
